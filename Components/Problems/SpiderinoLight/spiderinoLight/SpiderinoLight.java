0aee4cb8df1bba3825f24dc9966d1c7af625a92d
ZHEVZHYK <sergii.zhevzhyk@kapsch.net>
2017-10-18 00:24:17 +0200

    Adding the first template for the Spiderino and Light problem.

---
+++ Components/Problems/SpiderinoLight/spiderinoLight/SpiderinoLight.java
@@ -0,0 +1,43 @@
+package singlePong;
+
+import java.util.Hashtable;
+
+import core.AbstractRepresentation;
+import core.AbstractSingleProblem;
+import core.XMLFieldEntry;
+
+/**
+  * @author Sergii Zhevzhyk
+ *
+ */
+public class SpiderinoLight extends AbstractSingleProblem {
+
+	@Override
+	protected double evaluateCandidate(AbstractRepresentation candidate) {
+	}
+
+	/* (non-Javadoc)
+	 * @see core.AbstractSingleProblem#replayWithVisualization(core.AbstractRepresentation)
+	 */
+	@Override
+	public void replayWithVisualization(AbstractRepresentation candidate) {
+	}
+
+	@Override
+	public double getMaximumFitness() {
+		return Double.MAX_VALUE;
+	}
+	
+	@Override
+	public Hashtable<String, XMLFieldEntry> adjustRequirements(
+			Hashtable<String, XMLFieldEntry> requirements,
+			Hashtable<String, XMLFieldEntry> properties) {
+
+		XMLFieldEntry inputnumber = requirements.get("inputnumber");
+		int input = 3;
+		
+		inputnumber.setValue(Integer.toString(input));
+
+		return requirements;
+	}
+}
